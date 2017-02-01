6a7c2613b675b67e431e420a85d04b2951562f9f
syropian <collinpatrickhenderson@gmail.com>
2017-02-01 16:51:38 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Treegen.io</title>
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed" rel="stylesheet">
+    <link rel="stylesheet" href="/dist/styles.css">
+  </head>
+  <body>
+    <div class="app">
+      <header class="header">
+        <h3 class="logo"><a href="/">treegen.io</a></h3>
+      </header>
+      <div class="workspace">
+        <div class="workspace-pane editor">
+          <div class="pane-header"><h4>Editor</h4></div>
+          <textarea class="workspace-pane-textarea"></textarea>
+        </div>
+        <div class="workspace-pane output">
+          <div class="pane-header"><h4>Output</h4></div>
+          <textarea class="workspace-pane-textarea" readonly></textarea>
+        </div>
+      </div>
+    </div>
+    <script type="text/javascript" src="/dist/app.js">
+
+    </script>
+  </body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+let mix = require('laravel-mix').mix;
+
+mix.sass('src/styles.scss', 'dist/');
+
+// Full API
+// mix.js(src, output);
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.less(src, output);
+// mix.combine(files, destination);
+// mix.copy(from, to);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
